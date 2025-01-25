c1f3c6d0752c013a352d58089af2bd7389e1d3d4
jacques onek <onekongajaque@gmail.com>
2025-01-25 10:29:47 +0100

    je configure mon API TMDB

--- src/service/tmdb.ts
+++ src/service/tmdb.ts
@@ -1,4 +1,12 @@
 import axios  from "axios";
 
 const API_KEY = process.env.FILM_REACT_APP;
-const BASE_URL = ""
+const BASE_URL = "https://api.themoviedb.org/3"
+
+const tmdb = axios.create ({
+    baseURL:BASE_URL,
+    params:{
+        api_key:API_KEY,
+        language:"fr-FR"
+    }
+})
