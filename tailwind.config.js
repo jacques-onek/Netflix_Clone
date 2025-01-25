9b861cd5792048164b275cf24db85d782226015a
jacques onek <onekongajaque@gmail.com>
2025-01-25 19:12:21 +0100

    j'ajoute des font (typographie ) dans ma configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'Helvetica', 'sans-serif'], // Roboto comme principale
+      },
+    },
   },
   plugins: [],
 }
