d3a1b9cbfad272028ad7fb36b8a0397f18c151d7
onek james <onekongajaque@gmail.com>
2025-03-15 07:09:43 +0100

    je cree et develope le composant principale home le route index

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,8 +1,38 @@
+import NavBar from "../component/NavBar"
+import Header from "../component/Header"
+import { useQueries } from "@tanstack/react-query"
+import {fetchPopularTVShows,fetchPopularMovies} from '../service/tmdb'
+  import Scroller from "../component/Scroller"
 
 const Home = () => {
+
+  const results = useQueries({
+    queries:[
+      // fetching popular movies 
+      {
+        queryKey:["popularMovie"],
+        queryFn:fetchPopularMovies
+      },
+      {
+        queryKey:["popularTv"],
+        queryFn:fetchPopularTVShows
+      }
+    ]
+  })
+console.log(results[0].data)
+console.log(results[1].data)
   return (
     <div>
-      
+      {/* header section  */}
+       <div>
+         <Header/>
+         <NavBar/>
+       </div>
+       {/* main section  */}
+       <div>
+         <Scroller data={results} />
+       </div>
+       
     </div>
   )
 }
