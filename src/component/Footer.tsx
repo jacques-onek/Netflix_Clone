b2a18592c0018412fd4acc6a627ec962ef075495
onek james <onekongajaque@gmail.com>
2025-03-18 00:22:04 +0100

    je cree le footer

---
+++ src/component/Footer.tsx
@@ -0,0 +1,23 @@
+
+const Footer = () => {
+  return (
+    <div className="">
+      <div className=" px-8 py-16 max-md:px-4 text-center  ">
+        <p className="text-5xl mb-8 max-md:mb-10 font-black lg:text-4xl text-[#e40813] max-md:text-4xl  "> 
+            And much more,  Ready to move into the passionate <br />
+            <span>   wold of movies ? </span>
+        </p>
+        <p>
+            Checkout out the Plateform for more experience, Click the button below 
+        </p>
+          <button className="px-14 max-md:px-10 py-4 rounded-lg text-xl text-center my-7 bg-[#e40813] text-white">Go to the Plateform</button>
+      </div>
+      <div className="py-4 px-7 max-md:text-sm flex justify-around border-t-2 border-t-stone-900 text-[#e40813]">
+            <p>  Netflix 2.0  2024</p>
+            <p>By Onek James All right Reserved</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
