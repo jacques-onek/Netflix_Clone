f307c417041aab6ba18ddfc9a35577b9d3675102
onek james <onekongajaque@gmail.com>
2025-03-20 15:13:01 +0100

    je cree le scroler hub

---
+++ src/component/ScrollerHub.tsx
@@ -0,0 +1,60 @@
+import Card from "./cards/card"
+import CardTrending from "./cards/CardTrending"
+import CardTv from "./cards/CardTv"
+import SkeletonSlider from "./SkeletonSlider"
+import { useMediaData } from "../page/queries/UseMultipleQueries"
+
+
+
+const Scroller = () => {
+
+  const { isLoading, isError } = useMediaData();
+    
+
+
+  return (
+    <div className="relative h-auto flex flex-col px-10 max-md:px-4">
+
+
+        {/* slider pour les tranding  */}
+  {
+    isLoading ? <SkeletonSlider/> :
+
+      <div>
+         <h1 className="text-2xl  mt-14 mb-8 max-md:my-6"> Trending this week </h1>
+            <CardTrending />
+      </div>
+  }
+
+  {
+     isLoading ? <SkeletonSlider/> :
+
+            <div>
+                     <h1 className="text-2xl  mt-14 mb-8 max-md:my-6"> Popular Movies </h1>
+                     <Card />
+           </div> 
+  }
+
+{/* slider section series/tv   */}
+
+  {
+    isLoading ? <SkeletonSlider/> :
+
+      <div>
+         <h1 className="text-2xl  mt-14 mb-8 max-md:my-6"> Popular Series/tv </h1>
+            <CardTv />
+      </div>
+  }
+
+  {
+    isError && (
+      <div>
+         Network error, check yours network 
+      </div>
+    )
+  }
+    </div>
+  )
+}
+
+export default Scroller
