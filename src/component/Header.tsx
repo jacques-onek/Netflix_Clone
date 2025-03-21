fdeea502c8b81093bf3802566bcac6e4091c75b0
onek james <onekongajaque@gmail.com>
2025-03-21 12:16:44 +0100

    padding dans le header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="w-screen  top-2 h-[95vh] pl-14 bg-cover bg-center bg_header -z-10 max-md:auto">
        <div className="w-6/12 flex flex-col pt-44 px-10 font-Helvetica mb-14 max-md:w-screen max-lg:w-screen max-md:px-0 relative -left-8 max-md:pt-32">
-         <h1 className="text-6xl  font-black text-[#e40813] max-md:text-5xl max-lg:text-6xl ">The Perfect Touch for Your Hobbies !</h1>
+         <h1 className="text-6xl  font-black text-[#e40813] max-md:text-5xl max-md:pr-5 max-lg:text-6xl ">The Perfect Touch for Your Hobbies !</h1>
        </div>
        <div className="px-10 max-md:px-0 w-6/12 ml-10 max-md:w-full relative -left-16 max-lg:w-full overflow-x-auto">
       <p className="text-white w-full mb-14  max-md:text-xl max-md:mb-10 max-lg:text-3xl font-thin ">
