0841069bdde709565cfb824cd173f9a7afa2b00f
onek james <onekongajaque@gmail.com>
2025-03-22 07:23:36 +0100

    je delete quelques export

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,7 +1,5 @@
 export {default as Home} from "./Home"
 export {default as MovieHub} from "./MovieHub"
-export {default as MovieDetails} from "./ChannelDetails"
-export {default as TvHub} from "./TvHub"
-export {default as ChannelDetails} from "./ChannelDetails"
+export {default as MovieDetails} from "./MovieDetails"
 export {default as SeriesHub} from "./SeriesHub"
 export {default as SerieDetails} from "./SerieDetails"
